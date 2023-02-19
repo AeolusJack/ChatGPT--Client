a068b0c89e7706479922894005d8fa0b2b7f9aa6
AeolusJack <865494582@qq.com>
2023-02-19 12:46:35 +0800

    调测

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,11 +1,4 @@
-/**
- * 严肃声明：
- * 开源版本请务必保留此注释头信息，若删除我方将保留所有法律责任追究！
- * 本系统已申请软件著作权，受国家版权局知识产权以及国家计算机软件著作权保护！
- * 可正常分享和学习源码，不得用于违法犯罪活动，违者必究！
- * Copyright (c) 2020 陈尼克 all rights reserved.
- * 版权所有，侵权必究！
- */
+
 import axios from 'axios'
 import { showToast, showFailToast } from 'vant'
 import { setLocal } from '@/common/js/utils'
@@ -13,7 +6,7 @@ import router from '../router'
 
 console.log('import.meta.env', import.meta.env)
 
-axios.defaults.baseURL = import.meta.env.MODE == 'development' ? '//localhost/' : '//localhost/'
+axios.defaults.baseURL = import.meta.env.MODE == 'development' ? '//124.223.14.239/' : '//124.223.14.239/'
 axios.defaults.withCredentials = true
 axios.defaults.headers['X-Requested-With'] = 'XMLHttpRequest'
 axios.defaults.headers['token'] = localStorage.getItem('token') || ''
@@ -25,7 +18,7 @@ axios.interceptors.response.use(res => {
     return Promise.reject(res)
   }
   if (res.data.code == 'ACK') {
-   // console.log(res.data.data)
+    console.log(res.data.data)
     if (res.data.message) showFailToast(res.data.message)
     if (res.data.code == 'NACK') {
       router.push({ path: '/login' })
